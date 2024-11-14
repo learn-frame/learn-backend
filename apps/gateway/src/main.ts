e019dbc59c72408b3691cd9f655e40aa4da2a798
YanceyOfficial <yanceyofficial@gmail.com>
2024-11-14 19:11:00 +0800

    chore: delete useless code

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,20 +1,9 @@
 import LoggerModule from '@app/logger'
 import { NestFactory } from '@nestjs/core'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { GatewayModule } from './gateway.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule, { logger: LoggerModule })
-
-  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build()
-  const documentFactory = () => SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, documentFactory)
-
   await app.listen(10086)
 }
 bootstrap()
