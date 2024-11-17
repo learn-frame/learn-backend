e374bf643cd569e32abfead657834f70af9e647a
YanceyOfficial <yanceyofficial@gmail.com>
2024-11-17 16:22:07 +0800

    feat: show log on the terminal

---
+++ libs/logger/src/constant.ts
@@ -0,0 +1,7 @@
+export const levelsColors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  debug: 'blue',
+  verbose: 'cyan'
+}
--- libs/logger/src/index.ts
+++ libs/logger/src/index.ts
@@ -1,9 +1,12 @@
 import { ecsFormat } from '@elastic/ecs-winston-format'
+import chalk from 'chalk'
 import 'dotenv/config'
 import { WinstonModule } from 'nest-winston'
 import * as winston from 'winston'
 import DailyRotateFile from 'winston-daily-rotate-file'
 import { ElasticsearchTransport } from 'winston-elasticsearch'
+import { formatJSONDate } from 'yancey-js-util'
+import { levelsColors } from './constant'
 import elasticsearchTransportOptions from './elasticsearch.config'
 
 export default WinstonModule.createLogger({
@@ -28,12 +31,31 @@ export default WinstonModule.createLogger({
         maxSize: '20m',
         maxFiles: '30d'
       }),
+      new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.colorize({
+            colors: levelsColors
+          }),
+          winston.format.printf((info) => {
+            const symbols = Object.getOwnPropertySymbols(info)
+            const level: string = info[symbols[0]]
+            const color = levelsColors[level]
+            const chalkColor = chalk[color]
+            const logObj = JSON.parse(info[symbols[2]])
+            const message = chalkColor(
+              `[${level.toUpperCase()}] - ${formatJSONDate(logObj['@timestamp'])} [${logObj.context}] ${logObj.message}\n`
+            )
+            return message
+          })
+        ),
+        level: 'debug'
+      }),
+      new ElasticsearchTransport(elasticsearchTransportOptions)
       // new KafkaTransport({
       //   clientId: 'winston-kafka-logger',
       //   brokers: ['localhost:9092'],
       //   topic: 'logs-topic'
       // })
-      new ElasticsearchTransport(elasticsearchTransportOptions)
     ]
   })
 })
